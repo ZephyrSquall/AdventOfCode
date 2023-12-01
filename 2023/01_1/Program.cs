ba7a59ce322346be2ef1c174374e37cf557eeb9a
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2023-12-02 02:09:40 +0800

    Solved 2023 01_1

---
+++ 2023/01_1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines(args[0]);
+
+        int count = 0;
+
+        foreach (string line in lines)
+        {
+            bool foundFirstDigit = false;
+            int firstDigit = -1;
+            int lastDigit = -1;
+
+            foreach (char c in line)
+            {
+                if (Char.IsDigit(c))
+                {
+                    int value = (int)Char.GetNumericValue(c);
+                    lastDigit = value;
+                    if (!foundFirstDigit)
+                    {
+                        firstDigit = value;
+                        foundFirstDigit = true;
+                    }
+                }
+            }
+
+            int calibrationValue = 10 * firstDigit + lastDigit;
+            count += calibrationValue;
+        }
+
+        Console.WriteLine(count);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AdventOfCode
 My solutions to the Advent of Code puzzles.
+
+Solutions are written in C#. Run them with `dotnet run path-to-file.txt`, where `path-to-file.txt`
+is the relative path to the .txt file containing the puzzle input. I like to put the puzzle input in
+the same folder as the code under the name `input.txt`, in which case the command is `dotnet run
+input.txt`.
