8374cb1ba178aead573e1834302751cbc568d71f
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2023-12-13 21:45:50 +0800

    Added logging for solving 2023 12_2

--- 2023/Solvers/Day12Solver.cs
+++ 2023/Solvers/Day12Solver.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace AdventOfCode2023;
 
 class Day12Solver : Solver
@@ -12,14 +14,22 @@ class Day12Solver : Solver
         int validConfigurationSum = 0;
         string[] lines = File.ReadAllLines(PuzzleInputPath);
 
-        foreach (string line in lines)
+        //foreach (string line in lines)
+        for (int i = 0; i < lines.Length; i++)
         {
+            string line = lines[i];
+
             string[] splitLine = line.Split(' ');
             string springConditionRecord = splitLine[0];
             string contiguousGroupsLine = splitLine[1];
             int[] contiguousGroups = GetNumbersFromString(contiguousGroupsLine);
 
-            validConfigurationSum += GetNumberOfValidConfigurations(springConditionRecord, contiguousGroups, 0, 0);
+            int validConfigurationNum = GetNumberOfValidConfigurations(springConditionRecord, contiguousGroups, 0, 0);
+            validConfigurationSum += validConfigurationNum;
+
+            BigInteger nonogramConfigurationNum = GetNumberOfNonogramConfigurations(springConditionRecord, contiguousGroups);
+
+            Console.WriteLine($"{i} valid configs: {validConfigurationNum}, nonogram configs: {nonogramConfigurationNum}, difference: {nonogramConfigurationNum - validConfigurationNum} ({line})");
         }
 
         return validConfigurationSum;
@@ -27,11 +37,78 @@ class Day12Solver : Solver
 
     public override long SolvePart2()
     {
+        int validConfigurationSum = 0;
         string[] lines = File.ReadAllLines(PuzzleInputPath);
 
-        return 0;
+        // The index i is only used for logging debugging information. The foreach loop can be reinstated once logging is removed.
+        //foreach (string line in lines)
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+
+            string[] splitLine = line.Split(' ');
+            string springConditionRecord = splitLine[0];
+            string contiguousGroupsLine = splitLine[1];
+            int[] contiguousGroups = GetNumbersFromString(contiguousGroupsLine);
+
+            Unfold(ref springConditionRecord, ref contiguousGroups);
+
+            // Comment out this line for the sake of letting the code complete, as the number of configurations to check is too high them all individually in a reasonable time.
+            // It is unlikely that any method of exhaustively checking every combination will work without taking at least many hours to run, violating the principle that each puzzle has a solution that runs under 15 seconds.
+            // int validConfigurationNum =  GetNumberOfValidConfigurations(springConditionRecord, contiguousGroups, 0, 0);
+
+            // Set to 0 as a placeholder for now.
+            int validConfigurationNum = 0;
+            validConfigurationSum += validConfigurationNum;
+
+            // The number of nonogram configurations is found using combinatorics, so it still runs efficiently on these much-larger nonograms.
+            BigInteger nonogramConfigurationNum = GetNumberOfNonogramConfigurations(springConditionRecord, contiguousGroups);
+
+            // For logging only, update line to reflect being unfolded. Can delete this when logging is removed.
+            line = springConditionRecord + ", [" + contiguousGroups[0];
+            for (int j = 1; j < contiguousGroups.Length; j++)
+            {
+                line = String.Concat(line, ", ", contiguousGroups[j]);
+            }
+            line = line + "]";
+            Console.WriteLine($"{i}: valid configs: {validConfigurationNum}, nonogram configs: {nonogramConfigurationNum}, difference: {nonogramConfigurationNum - validConfigurationNum} ({line})");
+        }
+
+        return validConfigurationSum;
     }
 
+    // "nonogram configurations" treats the string as if it entirely consists of '?' and finds the number of solutions, essentially turning the problem into solving a row of a nonogram puzzle.
+    BigInteger GetNumberOfNonogramConfigurations(string springConditionRecord, int[] contiguousGroups)
+    {
+        // Get the number of slots that need to be filled with '#' or '.'
+        int slots = springConditionRecord.Length;
+        // Get the number of slots whose relative order is already predetermined according to the contiguous groups.
+        // This is every '#' (contiguousGroups.Sum()), plus a single '.' between each contiguous group to make sure each group is separated (contiguousGroups.Length - 1).
+        int filledSlots = contiguousGroups.Sum() + contiguousGroups.Length - 1;
+        // Get the remaining slots, whose relative order is not predetermined.
+        // These remaining slots will contain all '.', as every '#' has a relative order.
+        int slotsToFill = slots - filledSlots;
+        // Get the number of distinct positions the remaining '.' can be allocated to.
+        // These positions are before the first contiguous group (1), between each contiguous group ((contiguousGroups.Length - 1)), and after the last contiguous group (1), for total of (contiguousGroups.Length + 1).
+        int distinctPositions = contiguousGroups.Length + 1;
+
+        // The total number of nonogram solutions is now equal to the number of distinct multisets of cardinality slotsToFill with elements taken from a set of cardinality distinctPositions
+        // (We want to know how many ways we can assign "slotsToFill" elements to "distinctPositions" bins)
+        // This can be computed with Choose(n + k - 1, k), where k is the cardinality of the multiset to fill and n is the cardinality of the set to take elements from.
+        // See https://en.wikipedia.org/wiki/Combination#Number_of_combinations_with_repetition
+        return Choose(distinctPositions + slotsToFill - 1, slotsToFill);
+    }
+
+    BigInteger Factorial(int x)
+    {
+        if (x == 0) return 1;
+        else return x * Factorial(x - 1);
+    }
+
+    BigInteger Choose(int n, int k)
+    {
+        return Factorial(n) / (Factorial(k) * Factorial(n - k));
+    }
 
     void Unfold(ref string springConditionRecord, ref int[] contiguousGroups)
     {
@@ -52,6 +129,8 @@ class Day12Solver : Solver
         int currentDamagedSprings = springConditionRecord.Count(spring => spring == '#');
         int maximumDamagedSprings = contiguousGroups.Sum();
         int currentUnknownSprings = springConditionRecord.Count(spring => spring == '?');
+
+        //Console.WriteLine($"Checking {springConditionRecord}");
 
         // Check if the number of damaged springs exceed the total in the contiguous groups. If so, reject this branch.
         if (currentDamagedSprings > maximumDamagedSprings) return 0;
@@ -98,7 +177,7 @@ class Day12Solver : Solver
                     return GetNumberOfValidConfigurations(undamagedBranchConfiguration, contiguousGroups, springIndex + 1, contiguousGroupIndex);
                 }
             }
-            
+
             // If code reaches this part of the loop, the spring is an undamaged spring '.', which requires no special handling, so move to the next index and immediately check the spring again.
             springIndex++;
         }
