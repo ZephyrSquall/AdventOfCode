7e841202aa02928203c81e950c6d806e6f404732
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-03-30 01:36:29 +0800

    Solved 2015 03_1

--- 2015/Solvers/Day03Solver.cpp
+++ 2015/Solvers/Day03Solver.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
-#include <algorithm>
+#include <vector>
 #include "Day03Solver.h"
 
 int AdventOfCode2015::Day03Solver::SolvePart1()
@@ -11,9 +10,52 @@ int AdventOfCode2015::Day03Solver::SolvePart1()
     std::string line;
     std::getline(infile, line);
 
-    std::cout << line << std::endl;
+    struct coordinate
+    {
+        int x;
+        int y;
+    };
 
-    return 0;
+    int current_x = 0;
+    int current_y = 0;
+
+    std::vector<coordinate> coordinates;
+    coordinates.push_back(coordinate{.x = current_x, .y = current_y});
+
+    int housesVisited = 1;
+
+    for (int i = 0; i < line.size(); i++)
+    {
+        char instruction = line[i];
+
+        if (instruction == '^')
+            current_y++;
+        if (instruction == 'v')
+            current_y--;
+        if (instruction == '>')
+            current_x++;
+        if (instruction == '<')
+            current_x--;
+
+        bool visitedHouse = false;
+
+        for (int j = 0; j < coordinates.size(); j++)
+        {
+            if (coordinates[j].x == current_x && coordinates[j].y == current_y)
+            {
+                visitedHouse = true;
+                break;
+            }
+        }
+
+        if (!visitedHouse)
+        {
+            housesVisited++;
+            coordinates.push_back(coordinate{.x = current_x, .y = current_y});
+        }
+    }
+
+    return housesVisited;
 }
 
 int AdventOfCode2015::Day03Solver::SolvePart2()
