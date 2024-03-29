94c3e6f7b5c255bd5145d7aaceadbaf7d4e9e14e
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-03-29 11:57:44 +0800

    Solved 2015 01_2

--- 2015/Program.cpp
+++ 2015/Program.cpp
@@ -7,7 +7,11 @@ using namespace AdventOfCode2015;
 int main()
 {
     Day01Solver day01Solver;
+
     int day01Solution1 = day01Solver.SolvePart1();
     cout << day01Solution1 << endl;
+
+    int day01Solution2 = day01Solver.SolvePart2();
+    cout << day01Solution2 << endl;
     return 0;
 }
--- 2015/Solvers/Day01Solver.cpp
+++ 2015/Solvers/Day01Solver.cpp
@@ -13,10 +13,12 @@ int Day01Solver::SolvePart1()
 
     int floor = 0;
 
-    for(int i = 0; i < line.size(); i++)
+    for (int i = 0; i < line.size(); i++)
     {
-        if(line[i] == '(') floor++;
-        if(line[i] == ')') floor--;
+        if (line[i] == '(')
+            floor++;
+        if (line[i] == ')')
+            floor--;
     }
 
     return floor;
@@ -24,5 +26,27 @@ int Day01Solver::SolvePart1()
 
 int Day01Solver::SolvePart2()
 {
-    return 0;
+    ifstream puzzleInput("PuzzleInputs/01.txt");
+    string line;
+    getline(puzzleInput, line);
+
+    int floor = 0;
+    int firstBasementPosition = 0;
+
+    for (int i = 0; i < line.size(); i++)
+    {
+        if (line[i] == '(')
+            floor++;
+        if (line[i] == ')')
+            floor--;
+
+        if (floor < 0)
+        {
+            // Add 1 because string index starts at 0 and character position starts at 1.
+            firstBasementPosition = i + 1;
+            break;
+        }
+    }
+
+    return firstBasementPosition;
 }
