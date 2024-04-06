27f669950adbd436610e238dbada8d50b1734d60
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-04-06 21:26:19 +0800

    Solved 2015 06_2

--- 2015/Solvers/Day06Solver.cpp
+++ 2015/Solvers/Day06Solver.cpp
@@ -6,6 +6,65 @@
 #include <algorithm>
 #include "Day06Solver.h"
 
+enum class Action
+{
+    On,
+    Off,
+    Toggle
+};
+
+static void get_action(std::string line, Action &action, int start_coordinate[], int end_coordinate[])
+{
+    std::string actionless_line;
+
+    // The strings "turn on ", "turn off " and "toggle " only differ in their 7th character ('n', 'f', and ' ').
+    if (line[6] == 'n')
+    {
+        action = Action::On;
+        actionless_line = line.substr(8);
+    }
+    else if (line[6] == 'f')
+    {
+        action = Action::Off;
+        actionless_line = line.substr(9);
+    }
+    else
+    {
+        action = Action::Toggle;
+        actionless_line = line.substr(7);
+    }
+
+    // Extract x-y pairs from rest of line.
+    auto coordinates = actionless_line | std::ranges::views::split(std::string_view{" through "});
+    for (const auto &&[index, coordinates_view] : std::views::enumerate(coordinates))
+    {
+        auto coordinate_numbers = coordinates_view | std::ranges::views::split(',');
+
+        // for (int j = 0; j < 2; j++)
+        for (const auto &&[inner_index, coordinate_number_view] : std::views::enumerate(coordinate_numbers))
+        {
+
+            // Convert view to string.
+            std::string coordinate_number;
+            std::ranges::copy(coordinate_number_view | std::ranges::views::transform([](char c)
+                                                                                     { return (char)c; }),
+                              std::back_inserter(coordinate_number));
+
+            int number = stoi(coordinate_number);
+
+            switch (index)
+            {
+            case 0:
+                start_coordinate[inner_index] = number;
+                break;
+            case 1:
+                end_coordinate[inner_index] = number;
+                break;
+            }
+        }
+    }
+}
+
 int AdventOfCode2015::Day06Solver::SolvePart1()
 {
     std::ifstream infile("PuzzleInputs/06.txt");
@@ -20,67 +79,13 @@ int AdventOfCode2015::Day06Solver::SolvePart1()
         }
     }
 
-    enum class Action
-    {
-        On,
-        Off,
-        Toggle
-    };
-
     while (getline(infile, line))
     {
         Action action;
-        std::string actionless_line;
-
-        // The strings "turn on ", "turn off " and "toggle " only differ in their 7th character ('n', 'f', and ' ').
-        if (line[6] == 'n')
-        {
-            action = Action::On;
-            actionless_line = line.substr(8);
-        }
-        else if (line[6] == 'f')
-        {
-            action = Action::Off;
-            actionless_line = line.substr(9);
-        }
-        else
-        {
-            action = Action::Toggle;
-            actionless_line = line.substr(7);
-        }
-
         int start_coordinate[2];
         int end_coordinate[2];
 
-        // Extract x-y pairs from rest of line.
-        auto coordinates = actionless_line | std::ranges::views::split(std::string_view{" through "});
-        for (const auto &&[index, coordinates_view] : std::views::enumerate(coordinates))
-        {
-            auto coordinate_numbers = coordinates_view | std::ranges::views::split(',');
-
-            // for (int j = 0; j < 2; j++)
-            for (const auto &&[inner_index, coordinate_number_view] : std::views::enumerate(coordinate_numbers))
-            {
-
-                // Convert view to string.
-                std::string coordinate_number;
-                std::ranges::copy(coordinate_number_view | std::ranges::views::transform([](char c)
-                                                                                         { return (char)c; }),
-                                  std::back_inserter(coordinate_number));
-
-                int number = stoi(coordinate_number);
-
-                switch (index)
-                {
-                case 0:
-                    start_coordinate[inner_index] = number;
-                    break;
-                case 1:
-                    end_coordinate[inner_index] = number;
-                    break;
-                }
-            }
-        }
+        get_action(line, action, start_coordinate, end_coordinate);
 
         // Perform the corresponding action on the corresponding range.
         for (int i = start_coordinate[0]; i <= end_coordinate[0]; i++)
@@ -119,5 +124,52 @@ int AdventOfCode2015::Day06Solver::SolvePart1()
 
 int AdventOfCode2015::Day06Solver::SolvePart2()
 {
-    return 0;
+    std::cout << "test" << std::endl;
+    std::ifstream infile("PuzzleInputs/06.txt");
+    std::string line;
+
+    auto light_brightnesses = new int[1000][1000]();
+    
+    while (getline(infile, line))
+    {
+        Action action;
+        int start_coordinate[2];
+        int end_coordinate[2];
+
+        get_action(line, action, start_coordinate, end_coordinate);
+
+        // Perform the corresponding action on the corresponding range.
+        for (int i = start_coordinate[0]; i <= end_coordinate[0]; i++)
+        {
+            for (int j = start_coordinate[1]; j <= end_coordinate[1]; j++)
+            {
+                switch (action)
+                {
+                case Action::On:
+                    light_brightnesses[i][j]++;
+                    break;
+                case Action::Off:
+                    if (light_brightnesses[i][j] > 0)
+                    light_brightnesses[i][j]--;
+                    break;
+                case Action::Toggle:
+                    light_brightnesses[i][j] += 2;
+                    break;
+                }
+            }
+        }
+    }
+    
+    // Count lights
+    int total_brightness = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        for (int j = 0; j < 1000; j++)
+        {
+            total_brightness += light_brightnesses[i][j];
+        }
+    }
+
+    delete light_brightnesses;
+    return total_brightness;
 }
