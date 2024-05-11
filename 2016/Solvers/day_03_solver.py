801d62219015b502db76e70edceebcfaea61523c
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-05-11 19:11:26 +0800

    Solved 2016 03_2

--- 2016/Solvers/day_03_solver.py
+++ 2016/Solvers/day_03_solver.py
@@ -20,4 +20,22 @@ class Day03Solver(Solver):
 
 
     def solve_part_2() -> str:
-        return "0"
+        with open('PuzzleInputs/03.txt') as puzzle_input:
+            input_lines: list[str] = puzzle_input.read().splitlines()
+
+        possible_triangle_count: int = 0
+
+        for i in range(len(input_lines) // 3):
+            triangle_trios: list[list[int]] = [[], [], []]
+            for j in range(3):
+                input_line = input_lines[i*3 + j]
+                input_values: list[int] = list(map(int, input_line.split()))
+                triangle_trios[0].append(input_values[0])
+                triangle_trios[1].append(input_values[1])
+                triangle_trios[2].append(input_values[2])
+            for triangle_trio in triangle_trios:
+                triangle_trio.sort()
+                if (triangle_trio[0] + triangle_trio[1] > triangle_trio[2]):
+                    possible_triangle_count += 1
+
+        return str(possible_triangle_count)
