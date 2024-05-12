36c5277ed5a5d67a8d7036822b09b5467ef60d90
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-05-12 14:14:00 +0800

    Solved 2016 04_2

--- 2016/Solvers/day_04_solver.py
+++ 2016/Solvers/day_04_solver.py
@@ -4,53 +4,89 @@ from functools import cmp_to_key
 class Day04Solver(Solver):
     puzzle_title = "Security Through Obscurity"
 
-    def solve_part_1() -> str:
-        def letter_count_compare(a: tuple[chr, int], b: tuple[chr, int]) -> int:
-            if (a[1] < b[1]):
-                return -1
-            elif (a[1] > b[1]):
+    def _letter_count_compare(a: tuple[chr, int], b: tuple[chr, int]) -> int:
+        if (a[1] < b[1]):
+            return -1
+        elif (a[1] > b[1]):
+            return 1
+        else:
+            if (ord(a[0]) < ord(b[0])):
                 return 1
+            elif (ord(a[0]) > ord(b[0])):
+                return -1
             else:
-                if (ord(a[0]) < ord(b[0])):
-                    return 1
-                elif (ord(a[0]) > ord(b[0])):
-                    return -1
-                else:
-                    return 0
+                return 0
 
+    class EncryptedLine:
+        def __init__(self, input_line: str):
+            input_split_1: list[str] = input_line.split('[')
+            input_split_2: list[str] = input_split_1[0].rsplit('-', 1)
+
+            self.name: str = input_split_2[0]
+            self.sector_id: int = int(input_split_2[1])
+            # [:-1] gets all characters from a string except the last character, removing the
+            # trailing ']'.
+            self.checksum: str = input_split_1[1][:-1]
+
+        def has_valid_checksum(self) -> bool:
+            letter_counts: list[tuple[chr, int]] = []
+
+            for i in range(ord('a'),ord('z')+1):
+                letter: chr = chr(i)
+                letter_count: int = self.name.count(letter)
+                letter_counts.append((letter, letter_count))
+
+            letter_counts.sort(key=cmp_to_key(Day04Solver._letter_count_compare), reverse = True)
+
+            checksum: str = ""
+            for letter_count in letter_counts[:5]:
+                checksum += letter_count[0]
+
+            return checksum == self.checksum
+
+        def decrypt(self) -> str:
+            # Every 26 shifts returns the string to where it started, so only the remainder matters.
+            shift: int = self.sector_id % 26
+            decrypted_line: str = ""
+
+            for letter in self.name:
+                if (letter == '-'):
+                    decrypted_line += '-'
+                else:
+                    letter_index: int = ord(letter)
+                    letter_index += shift
+                    if (letter_index > ord('z')):
+                        letter_index -= 26
+
+                    shifted_letter: chr = chr(letter_index)
+                    decrypted_line += shifted_letter
+
+            return decrypted_line
+
+
+    def solve_part_1() -> str:
         with open('PuzzleInputs/04.txt') as puzzle_input:
             input_lines: list[str] = puzzle_input.read().splitlines()
 
         sector_id_sum: int = 0
 
         for input_line in input_lines:
-            input_split_1: list[str] = input_line.split('[')
-            # [:-1] gets all characters from a string except the last character, removing the
-            # trailing ']'.
-            given_checksum: str = input_split_1[1][:-1]
-            input_split_2: list[str] = input_split_1[0].rsplit('-', 1)
-            # Remove dashes from the name as they are not relevant to calculating the checksum.
-            name: str = input_split_2[0].replace('-', '')
-            sector_id: int = int(input_split_2[1])
-
-            letter_counts: list[tuple[chr, int]] = []
-
-            for i in range(ord('a'),ord('z')+1):
-                letter = chr(i)
-                letter_count = name.count(letter)
-                letter_counts.append((letter, letter_count))
-
-            letter_counts.sort(key=cmp_to_key(letter_count_compare), reverse = True)
-
-            calculated_checksum: str = ""
-            for letter_count in letter_counts[:5]:
-                calculated_checksum += letter_count[0]
-            
-            if (calculated_checksum == given_checksum):
-                sector_id_sum += sector_id
+            encrypted_line = Day04Solver.EncryptedLine(input_line)
+            if (encrypted_line.has_valid_checksum()):
+                sector_id_sum += encrypted_line.sector_id
 
         return str(sector_id_sum)
 
 
     def solve_part_2() -> str:
-        return "0"
+        with open('PuzzleInputs/04.txt') as puzzle_input:
+            input_lines: list[str] = puzzle_input.read().splitlines()
+
+        for input_line in input_lines:
+            encrypted_line = Day04Solver.EncryptedLine(input_line)
+            if (encrypted_line.has_valid_checksum()):
+                decrypted_line: str = encrypted_line.decrypt()
+                if "north" in decrypted_line and "pole" in decrypted_line:
+                    return str(encrypted_line.sector_id)
+
+        raise RuntimeError("North Pole storage room not found")
