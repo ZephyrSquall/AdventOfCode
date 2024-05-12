f2dabc09b11cc7954d16005b685b2adef5fea116
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-05-12 16:02:35 +0800

    Solved 2016 05_2

--- 2016/Solvers/day_05_solver.py
+++ 2016/Solvers/day_05_solver.py
@@ -17,7 +17,7 @@ class Day05Solver(Solver):
             # https://github.com/ZephyrSquall/AdventOfCode/blob/main/2015/Solvers/Day04Solver.cpp).
             # Converting this code to python would be incredibly tedious, so instead I'll simply use
             # a premade function for this.
-            hash_output = md5(hash_input.encode("utf-8")).hexdigest()
+            hash_output: str = md5(hash_input.encode("utf-8")).hexdigest()
 
             if (hash_output[:5] == "00000"):
                 password += hash_output[5]
@@ -28,4 +28,26 @@ class Day05Solver(Solver):
 
 
     def solve_part_2() -> str:
-        return "0"
+        with open('PuzzleInputs/05.txt') as puzzle_input:
+            input_line: str = puzzle_input.read()
+
+        password_characters: list[chr] = ['', '', '', '', '', '', '', '']
+        index: int = 0
+        is_password_digit_set: list[bool] = [False, False, False, False, False, False, False, False]
+
+        while (False in is_password_digit_set):
+            hash_input: str = input_line + str(index)
+            hash_output: str = md5(hash_input.encode("utf-8")).hexdigest()
+
+            if (hash_output[:5] == "00000"):
+                position_character: chr = hash_output[5]
+                if (position_character in ['0', '1', '2', '3', '4', '5', '6', '7']):
+                    position: int = int(position_character)
+                    if (not is_password_digit_set[position]):
+                        is_password_digit_set[position] = True
+                        password_characters[position] = hash_output[6]
+
+            index += 1
+
+        password = "".join(password_characters)
+        return password
