123456035adc948bcfc4d8d4d2f892983faa76c4
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-06-19 00:30:05 +0800

    Set up Rust for 2017 and solve first 3 days

---
+++ 2017/src/main.rs
@@ -0,0 +1,13 @@
+mod solver;
+
+fn main() {
+    println!("Day 01:");
+    println!("{}", solver::day_01::solve_part_1());
+    println!("{}", solver::day_01::solve_part_2());
+    println!("Day 02:");
+    println!("{}", solver::day_02::solve_part_1());
+    println!("{}", solver::day_02::solve_part_2());
+    println!("Day 03:");
+    println!("{}", solver::day_03::solve_part_1());
+    println!("{}", solver::day_03::solve_part_2());
+}
