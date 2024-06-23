7ff30dfbe2e505725d6f95ab5d1cab64c8aa702e
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-06-23 19:28:33 +0800

    Add timing for 2017 solutions

--- 2017/src/runner.rs
+++ 2017/src/runner.rs
@@ -1,13 +1,56 @@
 use std::fs;
+use std::fmt::Display;
+use std::time::Instant;
+use crate::solver::SOLVERS;
+
+struct Solution<'a> {
+    day: u8,
+    title: &'a str,
+    solution_1: Box<dyn Display>,
+    solution_2: Box<dyn Display>,
+    time_1: u128,
+    time_2: u128,
+}
 
 pub fn run() {
-    for solver in crate::solver::SOLVERS {
+    let solutions = run_solvers();
+
+    for solution in solutions {
+        println!("Day {}: {}", solution.day, solution.title);
+        println!("{} in {} ns", solution.solution_1, solution.time_1);
+        println!("{} in {} ns", solution.solution_2, solution.time_2);
+    }
+}
+
+fn run_solvers() -> Vec<Solution<'static>> {
+    let mut solutions = Vec::new();
+
+    for solver in SOLVERS {
         // "{:02}" left-pads the day number with a 0 if needed so the width of the number is two.
         let file_path = format!("puzzle_inputs/{:02}.txt", solver.day);
         let input = fs::read_to_string(&file_path).expect("Error reading file");
 
-        println!("Day {}", solver.day);
-        println!("{}", (solver.solve_part_1)(&input));
-        println!("{}", (solver.solve_part_2)(&input));
+        let start = Instant::now();
+        let solution_1 = (solver.solve_part_1)(&input);
+        let duration = start.elapsed();
+        let time_1 = duration.as_micros();
+
+        let start = Instant::now();
+        let solution_2 = (solver.solve_part_2)(&input);
+        let duration = start.elapsed();
+        let time_2 = duration.as_nanos();
+
+        let solution = Solution {
+            day: solver.day,
+            title: solver.title,
+            solution_1,
+            solution_2,
+            time_1,
+            time_2,
+        };
+
+        solutions.push( solution);
     }
+
+    solutions
 }
