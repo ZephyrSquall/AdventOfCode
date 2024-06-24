4ef535076e0ee003b6afe6de757d13f21c5e22fd
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-06-24 21:04:36 +0800

    Solved 2017 05_2

--- 2017/src/solver/day_05.rs
+++ 2017/src/solver/day_05.rs
@@ -4,30 +4,36 @@ pub const SOLVER: Solver = Solver {
     day: 5,
     title: "A Maze of Twisty Trampolines, All Alike",
 
-    solve_part_1: |input| {
-        let mut jumps = Vec::new();
-        for jump in input.lines() {
-            let jump = jump.parse::<isize>().expect("Error parsing number");
-            jumps.push(jump);
-        }
-
-        let mut step = 0;
-        let mut position: isize = 0;
-
-        loop {
-            match jumps.get_mut(position as usize) {
-                Some(jump) => {
-                    position += *jump;
-                    *jump += 1;
-                    step += 1;
-                }
-                None => return Solution::U32(step),
-            }
-        }
-    },
-
-    solve_part_2: |input| Solution::U32(0),
+    solve_part_1: |input| solve(input, false),
+    solve_part_2: |input| solve(input, true),
 };
+
+fn solve(input: &str, strange_jumps: bool) -> Solution {
+    let mut jumps = Vec::new();
+    for jump in input.lines() {
+        let jump = jump.parse::<isize>().expect("Error parsing number");
+        jumps.push(jump);
+    }
+
+    let mut step = 0;
+    let mut position: isize = 0;
+
+    loop {
+        match jumps.get_mut(position as usize) {
+            Some(jump) => {
+                step += 1;
+                position += *jump;
+
+                if strange_jumps && *jump >= 3 {
+                    *jump -= 1;
+                } else {
+                    *jump += 1;
+                }
+            }
+            None => return Solution::U32(step),
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -44,5 +50,18 @@ mod test {
 -3",
         );
         assert_eq!(Solution::U32(5), result)
+    }
+
+    #[test]
+    fn part2_example1() {
+        let result = (SOLVER.solve_part_2)(
+            "\
+0
+3
+0
+1
+-3",
+        );
+        assert_eq!(Solution::U32(10), result)
     }
 }
