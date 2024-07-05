680230d219d7a7a45acb260e7be18107ddb8c36a
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-07-05 18:26:51 +0800

    Solved 2017 11_2

--- 2017/src/solver/day_11.rs
+++ 2017/src/solver/day_11.rs
@@ -14,60 +14,81 @@ pub const SOLVER: Solver = Solver {
         let mut hex_y: i32 = 0;
 
         for direction in input.split(',') {
-            match direction {
-                "n" => {
-                    hex_y += 1;
-                }
-                "ne" => {
-                    hex_x += 1;
-                }
-                "se" => {
-                    hex_x += 1;
-                    hex_y -= 1;
-                }
-                "s" => {
-                    hex_y -= 1;
-                }
-                "sw" => {
-                    hex_x -= 1;
-                }
-                "nw" => {
-                    hex_x -= 1;
-                    hex_y += 1;
-                }
-                _ => {
-                    panic!("Invalid direction");
-                }
+            step(direction, &mut hex_x, &mut hex_y);
+        }
+
+        Solution::I32(get_distance(hex_x, hex_y))
+    },
+
+    solve_2: |input| {
+        let mut hex_x: i32 = 0;
+        let mut hex_y: i32 = 0;
+        let mut furthest_distance = 0;
+
+        for direction in input.split(',') {
+            step(direction, &mut hex_x, &mut hex_y);
+
+            let distance = get_distance(hex_x, hex_y);
+            if distance > furthest_distance {
+                furthest_distance = distance;
             }
         }
 
-        // A southeast or northwest step shifts along the x- and y- axes simultaneously, so they
-        // offer "shortcuts" back to the center hex that would be missed if movement were restricted
-        // along only the x- and y- axes. So prioritize southeast or northwest steps by handling
-        // them first.
-        let mut extra_steps = 0;
-
-        // Move northeast for as long as it's possible.
-        while hex_x > 0 && hex_y < 0 {
-            extra_steps += 1;
-            hex_x -= 1;
-            hex_y += 1;
-        }
-
-        // Move southwest for as long as it's possible.
-        while hex_x < 0 && hex_y > 0 {
-            extra_steps += 1;
-            hex_x += 1;
-            hex_y -= 1;
-        }
-
-        let steps = hex_x.abs() + hex_y.abs() + extra_steps;
-
-        Solution::I32(steps)
+        Solution::I32(furthest_distance)
     },
-
-    solve_2: |input| Solution::U8(0),
 };
+
+fn step(direction: &str, hex_x: &mut i32, hex_y: &mut i32) {
+    match direction {
+        "n" => {
+            *hex_y += 1;
+        }
+        "ne" => {
+            *hex_x += 1;
+        }
+        "se" => {
+            *hex_x += 1;
+            *hex_y -= 1;
+        }
+        "s" => {
+            *hex_y -= 1;
+        }
+        "sw" => {
+            *hex_x -= 1;
+        }
+        "nw" => {
+            *hex_x -= 1;
+            *hex_y += 1;
+        }
+        _ => {
+            panic!("Invalid direction");
+        }
+    }
+}
+
+fn get_distance(mut hex_x: i32, mut hex_y: i32) -> i32 {
+    // A southeast or northwest step shifts along the x- and y- axes simultaneously, so they
+    // offer "shortcuts" back to the center hex that would be missed if movement were restricted
+    // along only the x- and y- axes. So prioritize southeast or northwest steps by handling
+    // them first.
+    let mut extra_steps = 0;
+
+    // Move northeast for as long as it's possible.
+    while hex_x > 0 && hex_y < 0 {
+        extra_steps += 1;
+        hex_x -= 1;
+        hex_y += 1;
+    }
+
+    // Move southwest for as long as it's possible.
+    while hex_x < 0 && hex_y > 0 {
+        extra_steps += 1;
+        hex_x += 1;
+        hex_y -= 1;
+    }
+
+    hex_x.abs() + hex_y.abs() + extra_steps
+}
 
 #[cfg(test)]
 mod test {
