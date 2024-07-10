a6899426ef33f57090307cd4fb20122c5e8cf45a
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-07-10 22:37:54 +0800

    Solved 2017 15_2

--- 2017/src/solver/day_15.rs
+++ 2017/src/solver/day_15.rs
@@ -5,25 +5,7 @@ pub const SOLVER: Solver = Solver {
     title: "Dueling Generators",
 
     solve_1: |input| {
-        let mut lines = input.lines();
-        // Get the next line of the puzzle input, split it into individual words, take the last
-        // word, and convert it to an integer.
-        let mut generator_a = lines
-            .next()
-            .expect("Input should have first line")
-            .split_whitespace()
-            .next_back()
-            .expect("First line should have text")
-            .parse()
-            .expect("First line should end with valid number");
-        let mut generator_b = lines
-            .next()
-            .expect("Input should have second line")
-            .split_whitespace()
-            .next_back()
-            .expect("Second line should have text")
-            .parse()
-            .expect("Second line should end with valid number");
+        let (mut generator_a, mut generator_b) = get_generator_starting_values(input);
 
         fn generate_next(value: u64, factor: u64) -> u64 {
             (value * factor) % 2147483647
@@ -47,8 +29,59 @@ pub const SOLVER: Solver = Solver {
         Solution::U32(judge_count)
     },
 
-    solve_2: |input| Solution::U8(0),
+    solve_2: |input| {
+        let (mut generator_a, mut generator_b) = get_generator_starting_values(input);
+
+        // generate_next now takes a closure as a third argument that returns true when the
+        // generator's condition is satisfied.
+        fn generate_next<F: Fn(u64) -> bool>(mut value: u64, factor: u64, condition: F) -> u64 {
+            loop {
+                value = (value * factor) % 2147483647;
+                if condition(value) {
+                    return value;
+                }
+            }
+        }
+
+        let mut judge_count = 0;
+
+        for _ in 0..5000000 {
+            generator_a = generate_next(generator_a, 16807, |v| v % 4 == 0);
+            generator_b = generate_next(generator_b, 48271, |v| v % 8 == 0);
+
+            if generator_a & 0xffff == generator_b & 0xffff {
+                judge_count += 1;
+            }
+        }
+
+        Solution::U32(judge_count)
+    },
 };
+
+fn get_generator_starting_values(input: &str) -> (u64, u64) {
+    let mut lines = input.lines();
+
+    // Get the next line of the puzzle input, split it into individual words, take the last
+    // word, and convert it to an integer.
+    let generator_a = lines
+        .next()
+        .expect("Input should have first line")
+        .split_whitespace()
+        .next_back()
+        .expect("First line should have text")
+        .parse()
+        .expect("First line should end with valid number");
+    let generator_b = lines
+        .next()
+        .expect("Input should have second line")
+        .split_whitespace()
+        .next_back()
+        .expect("Second line should have text")
+        .parse()
+        .expect("Second line should end with valid number");
+
+    (generator_a, generator_b)
+}
 
 #[cfg(test)]
 mod test {
@@ -63,6 +96,18 @@ Generator A starts with 65
 Generator B starts with 8921"
             ),
             Solution::U16(588)
+        )
+    }
+
+    #[test]
+    fn example2_1() {
+        assert_eq!(
+            (SOLVER.solve_2)(
+                "\
+Generator A starts with 65
+Generator B starts with 8921"
+            ),
+            Solution::U16(309)
         )
     }
 }
