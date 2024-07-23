c06bab85df09e414c9d799e63b1e3dce55ad40d6
Cameron Smith <145758512+ZephyrSquall@users.noreply.github.com>
2024-07-23 18:44:25 +0800

    Solved 2017 23_2

--- 2017/src/solver/day_23.rs
+++ 2017/src/solver/day_23.rs
@@ -29,34 +29,146 @@ pub const SOLVER: Solver = Solver {
         Solution::U32(program_state.mul_invocations)
     },
 
-    #[allow(unused_variables, unreachable_code)]
     solve_2: |input| {
-        // I currently cannot find a solution to this puzzle so I am skipping it for now. My current
-        // attempt did not finish after running for several hours so it is clearly not the intended
-        // solution, but I currently cannot think of any way to optimize my solution and am stumped.
-        // So that other solutions aren't blocked, a return statement has been inserted at the
-        // start of this solver to prevent the rest of it from executing.
-        return Solution::U8(0);
+        // This puzzle required making some extreme assumptions about the puzzle input. By stepping
+        // through my puzzle input step-by-step and mapping out the logic, I determined that the
+        // initial values in the b and c registers define an inclusive lower and upper bound for a
+        // range of values, which are offset from each other by a hardcoded value in the
+        // "sub b -<SOME VALUE>" instruction on the second-last line (e.g. if b is 4, c is 10, and
+        // the hardcoded value is 2, the values that the program would iterate over are be 4, 6, 8,
+        // 10). For each of these values, it sets register f to 1, then it creates a sub-loop using
+        // register d, which itself has its own sub-loop using register e, both of which count from
+        // 2 to the current value. In every iteration of the innermost loop, it checks if
+        // d*e - b == 0, and if this is true, it sets f to 0. Once the outer sub-loop completes, if
+        // f was set to 0, register h is incremented by 1.
+        //
+        // The condition d*e = b == 0 is equivalent to d*e == b. If this is never true, then
+        // register f remains set to 1 and h isn't incremented; if it is true at least one time,
+        // then register f is set to 0 and h gets incremented. As such, for each value, the program
+        // essentially asks "Does there exist any two integers which are both greater than or equal
+        // to 2, such that their product equals the given value?" This is equivalent to asking if
+        // the given value is a composite number (a positive integer other than 1 which is not a
+        // prime number). As such, what the program ultimately does is count every composite number
+        // within the set of numbers defined by the bounds of registers b and c and the hardcoded
+        // offset, and stores the count in register h.
+        //
+        // I assume this is the intended trick of the puzzle, and that every puzzle input is set up
+        // to follow the rules described above. As such, my solution searches the program in the
+        // puzzle input for every line that modifies registers b or c, executes all of them except
+        // for the last one, assumes the values for b and c I end up with are the initial values for
+        // them, and assumes the literal argument to the last instruction instead defines the
+        // offset. I ignore all other lines in the puzzle input, assuming that the rest of it is
+        // just implementation details for counting composite numbers. I use the extracted b, c, and
+        // offset values to set up the full list of values, then use my own implementation for
+        // counting composite numbers. If any of the assumptions I have made for this solution are
+        // incorrect, then my solution might not work for all possible puzzle inputs.
 
-        let instructions = get_instructions(input);
+        fn is_prime(x: u32) -> bool {
+            // Technically, an is_prime() function would need to check if the input is 1 or lower
+            // and immediately return false if so, but it's known this function will never be ran
+            // for a value smaller than 2 within this solver, so this check is omitted.
+
+            // When checking for prime numbers, only integers up to the square root of the number
+            // need to be checked (any factor greater than the square root will need to be
+            // multiplied by a factor smaller than the square root to equal the given number, so if
+            // the number isn't prime, it will always have at least one factor (other than 1) that
+            // is smaller than its square root).
+
+            // Intentionally truncating the float (this is accounted for by using an inclusive range
+            // "..=" which includes the end instead of a normal range ".." which excludes the end).
+            #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+            for test in 2..=(f64::from(x)).sqrt() as u32 {
+                if x % test == 0 {
+                    return false;
+                }
+            }
+            true
+        }
+
+        // Predicate that says whether the given instruction will modify the value in the b or c
+        // registers.
+        fn is_writing_to_b_or_c(instruction: &Instruction) -> bool {
+            match instruction {
+                Instruction::Set(op_1, _)
+                | Instruction::Sub(op_1, _)
+                | Instruction::Mul(op_1, _) => {
+                    if let RegisterOrValue::Register(index) = op_1 {
+                        // Registers b and c have indexes 1 and 2 respectively.
+                        return *index == 1 || *index == 2;
+                    }
+                    false
+                }
+                // The jnz function doesn't modify any registers so always return false (in
+                // principle they might affect b and c through looping around another instruction
+                // that affects it, however since only the initial values of b and c are needed,
+                // it's assumed that once jumping is done to begin looping, the initial values of b
+                // and c have been finalized and this jnz functions can still be safely ignored).
+                Instruction::Jnz(_, _) => false,
+            }
+        }
+
+        let mut instructions = get_instructions(input);
+        // Remove all instructions that don't modify b or c.
+        instructions.retain(is_writing_to_b_or_c);
+
         let mut program_state = ProgramState {
             program_counter: 0,
             registers: [1, 0, 0, 0, 0, 0, 0, 0],
             mul_invocations: 0,
         };
 
-        while let Some(instruction) = instructions.get(program_state.program_counter) {
+        let mut instruction_iter = instructions.iter();
+
+        // First take the last value off the list of instructions and use it to find the offset.
+        let last_instruction = instruction_iter
+            .next_back()
+            .expect("Instructions should have last element");
+        let offset = if let Instruction::Sub(_, op_2) = last_instruction {
+            if let RegisterOrValue::Value(value) = op_2 {
+                // value is expected to be negative as it's used in a sub instruction with the
+                // intention of adding an amount to register b, so the absolute value of it must be
+                // taken.
+                value.unsigned_abs()
+            } else {
+                panic!("Offset should be an integer literal")
+            }
+        } else {
+            panic!("Last instruction should be a sub instruction to add the offset")
+        };
+
+        // Execute all instructions except the last one (which is no longer in instruction_iter).
+        for instruction in instruction_iter {
             instruction.execute(&mut program_state);
         }
 
-        // Register 'h' is the 8th register, so it has index 7.
-        Solution::I64(program_state.registers[7])
+        // It's expected that these values are positive. unsigned_abs() helps convert to u32.
+        // Register b:
+        let mut range_start = program_state.registers[1].unsigned_abs();
+        // Register c:
+        let range_end = program_state.registers[2].unsigned_abs();
+
+        let mut values_in_range = Vec::with_capacity(1001);
+
+        while range_start <= range_end {
+            values_in_range.push(range_start);
+            range_start += offset;
+        }
+
+        // Count composite numbers.
+        let mut composites = 0;
+        for value in values_in_range {
+            if !is_prime(value) {
+                composites += 1;
+            }
+        }
+
+        return Solution::U32(composites);
     },
 };
 
 struct ProgramState {
     program_counter: usize,
-    registers: [i64; 8],
+    registers: [i32; 8],
     mul_invocations: u32,
 }
 // Note that unlike in day 18, it is specified that only the first 8 letters ('a' to 'h')
@@ -64,7 +176,7 @@ struct ProgramState {
 // ints, rather than a HashMap. Hence the Register enum uses a usize instead of a char here.
 enum RegisterOrValue {
     Register(usize),
-    Value(i64),
+    Value(i32),
 }
 // Representation of individual instructions.
 enum Instruction {
@@ -78,7 +190,7 @@ impl RegisterOrValue {
     // Get the value from the operand, either by directly returning it if the operand is an
     // integer literal, or by fetching the value from the register if the operand is a
     // register name.
-    fn get_value(&self, program_state: &ProgramState) -> i64 {
+    fn get_value(&self, program_state: &ProgramState) -> i32 {
         match self {
             RegisterOrValue::Register(index) => program_state.registers[*index],
             RegisterOrValue::Value(value) => *value,
@@ -88,7 +200,7 @@ impl RegisterOrValue {
     // only be called for operands that are going to be written to (first operand of the
     // set, sub, and mul instructions), which only makes sense when the operand is a
     // register, so this function panics if the operand is a literal.
-    fn get_value_mut<'a>(&self, program_state: &'a mut ProgramState) -> &'a mut i64 {
+    fn get_value_mut<'a>(&self, program_state: &'a mut ProgramState) -> &'a mut i32 {
         match self {
             RegisterOrValue::Register(index) => &mut program_state.registers[*index],
             RegisterOrValue::Value(_) => {
